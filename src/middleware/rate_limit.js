75fb04e2bc70dc4d9ffd5a7c4de62f7aae6ff0a9
N-Sangeetha <nsangeetha2517@gmail.com>
2023-04-21 10:13:11 +0530

    memoize function

--- src/middleware/rate_limit.js
+++ src/middleware/rate_limit.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit')
 
 const rate_limiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 1,
-  message: {code: 429, message: 'You have exceeded the 10 requests in 24 hrs limit!'}, 
+  max: 100,
+  message: {code: 429, message: 'You have exceeded the 100 requests in 24 hrs limit!'}, 
   standardHeaders: true,
 })
 

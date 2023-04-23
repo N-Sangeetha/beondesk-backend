8a57ada58ab3618b73539b53e66d43c8837784b4
N-Sangeetha <nsangeetha2517@gmail.com>
2023-04-23 15:00:40 +0530

    structure change

--- src/routers/image.router.js
+++ src/routers/image.router.js
@@ -1,71 +1,13 @@
 const express = require("express");
+const router = new express.Router();
 const auth = require("../middleware/auth");
 const rate_limiter = require("../middleware/rate_limit");
-const axios = require("axios");
-const NodeCache = require("node-cache");
-
-const router = new express.Router();
-const cache = new NodeCache();
+const imageController = require("../controller/image.controller")
 
 //images list
-router.get("/images", [auth, rate_limiter], async (req, res) => {
-  try {
-    let page = req.query.page || 1;
-    let per_page = req.query.per_page || 9;
-    let query = req.query.query || "hills";
-
-    let images = null;
-
-    if (cache.has("cacheImage")) {
-      console.log("from cache");
-
-      const cacheImages = cache.get("cacheImage");
-      let index = cacheImages?.findIndex(
-        (x) => x.page === page && x.per_page === per_page && x.query === query
-      );
-      if (index !== -1) {
-        return res.status(200).send({ code: 200, images: cacheImages[index].images });
-      }
-    }
-
-    console.log("from api");
-    const response = await axios.get(
-      `https://api.unsplash.com/search/photos?client_id=${
-        process.env.UNSPLASH_ACCESS_KEY
-      }&page=${page}&per_page=${per_page}${query ? `&query=${query}` : ""}`
-    );
-    images = response.data;
-
-    let cacheImages = cache.get("cacheImage");
-    cacheImages = cacheImages ? cacheImages : []
-    cacheImages.push({ page, per_page, query, images })
-
-    cache.set("cacheImage", cacheImages);
-
-    return res.status(200).send({ code: 200, images: images });
-  } catch (e) {
-    console.log("Error from images api", e);
-    res.status(500).send({ code: 500, message: e.name });
-  }
-});
+router.get("/images", [auth, rate_limiter],imageController.getImages);
 
 //image download
-router.get("/download", auth, async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.unsplash.com/photos/${req.query.id}/download?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
-    )
-
-    if(response.data){
-      res.status(200).send(response.data?.url);
-    }else{
-      res.status(404).send('Not Found')
-    }
-
-  } catch (e) {
-    console.log('Error in download api', e)
-    res.status(500).send({ code: 500, message: e.name });
-  }
-});
+router.get("/download", auth, imageController.downloadImage);
 
 module.exports = router;
